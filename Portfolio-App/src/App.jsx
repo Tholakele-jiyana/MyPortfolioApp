530dd52eb6ff7a9b6ea103072036ab199dae186d
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-10-23 12:47:38 +0200

    imported components  onto App.jsx

---
+++ Portfolio-App/src/App.jsx
@@ -0,0 +1,7 @@
+import Header "./Components/Header";
+import About "./Components/About";
+import Hero "./Components/Hero";
+import Projects "./Components/Projects";
+import Footer "./Components/Footer"; 
+import Home "./Components/Home";
+import Contact "./Components/Contact";
