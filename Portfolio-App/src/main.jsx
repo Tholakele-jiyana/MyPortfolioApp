ab1f2e27ceb47a5067d0561c92eb1f90fcf0e2b9
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-10-26 23:48:40 +0200

    Comment out Import index.css because page is not responding anymore

---
+++ Portfolio-App/src/main.jsx
@@ -0,0 +1,10 @@
+import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+//import "./index.css";
+import React from "react";
+
+const root = ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+  <App />
+  </React.StrictMode>
+  );
