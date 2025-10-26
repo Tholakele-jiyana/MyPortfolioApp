d0b95d606997096959851722c13c82c575170b08
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-10-27 00:12:36 +0200

    Wrote a brief decription in the About section

--- Portfolio-App/src/Components/About.jsx
+++ Portfolio-App/src/Components/About.jsx
@@ -2,6 +2,15 @@ function About() {
   return (
     <div>
         <h1>About</h1>
+        <p>I am a Software Development student currently enrolled at Melsoft Academy. 
+            I am inquisitive about the evolving digital space which is why I decided to pursue a career in tech. 
+            I enjoy learning about the new technologies and how they have become useful in solving
+            real world problems. 
+            The desire to shift from my previous career to tech is both exciting and challenging.
+            I am looking forward to gaining more knowledge as I progress and finding my niche in the field. 
+
+            Outside of my day-to-day job and studies, I enjoy spending time with family and friends over a good meal.
+        </p>
     </div>
   );
 }
