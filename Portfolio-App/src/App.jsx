6b7aa77ade52036517e4118de8d6423443c8a007
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-10-27 00:00:30 +0200

    Added description about the Header Section

--- Portfolio-App/src/App.jsx
+++ Portfolio-App/src/App.jsx
@@ -4,20 +4,19 @@ import About from "./Components/About";
 import Hero from "./Components/Hero";
 import Projects from "./Components/Projects";
 import Footer from "./Components/Footer"; 
-import Home from"./Components/Home";
 import Contact from "./Components/Contact";
 
 //Export the App components that uses the imported components
-export default function App() {
+function App() {
   return (
     <div>
       <Header />
       <About />
       <Hero />
       <Projects />
-      <Footer />
-      <Home />
       <Contact />
+      <Footer />
     </div>
   );
 }
+export default App;
--- Portfolio-App/src/Components/Header.jsx
+++ Portfolio-App/src/Components/Header.jsx
@@ -1,7 +1,14 @@
 function Header() {
   return (
     <div>
-        <h1>Header</h1>
+        <h1>Header-NavBar Content</h1>
+        <p>This would ideally be the section where I put the navigation link</p>
+        <p>This will include all the components I have added:</p>
+        <h3>About</h3>
+        <h3>Hero</h3>
+        <h3>Projects</h3>
+        <h3>Contact</h3>
+        
     </div>
   );
 }
