1fdbabc296756f1fe96693b378c3e4a8d13f0f10
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-10-26 22:09:10 +0200

    Export components added on App.jsx

--- Portfolio-App/src/App.jsx
+++ Portfolio-App/src/App.jsx
@@ -1,3 +1,4 @@
+//Import the components to be used in the App component
 import Header from "./Components/Header";
 import About from "./Components/About";
 import Hero from "./Components/Hero";
@@ -5,3 +6,18 @@ import Projects from "./Components/Projects";
 import Footer from "./Components/Footer"; 
 import Home from"./Components/Home";
 import Contact from "./Components/Contact";
+
+//Export the App components that uses the imported components
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <About />
+      <Hero />
+      <Projects />
+      <Footer />
+      <Home />
+      <Contact />
+    </div>
+  );
+}
