fe20c47f94956e7c34d477c89291fe7dd47d7dd7
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-10-29 20:43:49 +0200

    Changed order of components

--- Portfolio-App/src/App.jsx
+++ Portfolio-App/src/App.jsx
@@ -3,15 +3,16 @@ import Header from "./Components/Header";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Projects from "./Components/Projects";
-import Footer from "./Components/Footer"; 
 import Contact from "./Components/Contact";
+import Footer from "./Components/Footer"; 
+
 
 //Export the App components that uses the imported components
 function App() {
   return (
     <div>
       <Header />
-       <Home />
+      <Home />
       <About />
       <Projects />
       <Contact />
