6e9c18cefb5e4155796130abd943e151a67e605a
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-10-30 00:22:33 +0200

    made edits on the footer

--- Portfolio-App/src/Components/Footer.jsx
+++ Portfolio-App/src/Components/Footer.jsx
@@ -1,9 +1,20 @@
+
+
 function Footer() {
   return (
-    <div>
-        <h1>Footer</h1>
-        <p>the footer section will include relevant links and information.</p>
-    </div>
+    <footer className="bg-gray-900 text-gray-300 py-6">
+      <div className="max-w-7xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
+        
+        <p className="text-sm mb-4 md:mb-0">
+           LT-Portfoloio. All rights reserved.
+        </p>
+        
+        <div className="flex space-x-4">
+          <a href="#" className="hover:text-white text-sm">Home</a>
+        </div>
+        
+      </div>
+    </footer>
   );
 }
 
