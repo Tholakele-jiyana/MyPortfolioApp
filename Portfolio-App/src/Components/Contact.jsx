9dfb69ef9fd637460d6c8a95116988e799d38c1b
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-10-30 00:14:32 +0200

    Made edits on the contact me description and made the sign up from to be a "send message" or inquiry form

--- Portfolio-App/src/Components/Contact.jsx
+++ Portfolio-App/src/Components/Contact.jsx
@@ -9,10 +9,12 @@ function Contact() {
           {/* Left text section */}
           <div className="text-center lg:text-left lg:w-7/12 mb-12 lg:mb-0">
             <h1 className="text-4xl sm:text-5xl font-bold leading-tight mb-4">
-              Vertically centered hero sign-up form
+             Get in touch with me
             </h1>
             <p className="text-lg text-gray-700 lg:w-10/12">
-              Below is an example form built entirely with form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.
+              You can reach me via email at tholakelejiyana@gmail.com <br />
+              Or mobile number at +27 79 632 940 <br />
+                Alternatively, fill out the form to send me a message directly for a quote or inquiry.
             </p>
           </div>
 
@@ -30,28 +32,18 @@ function Contact() {
               </div>
 
               <div className="mb-4">
-                <label htmlFor="password" className="block text-gray-700 mb-1">Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="Password"
+                <label htmlFor="message" className="block text-gray-700 mb-1">Message</label>
+                <textarea
+                  id="message"
+                  placeholder="Your message"
                   className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none"
                 />
               </div>
 
-              <div className="mb-4 flex items-center">
-                <input type="checkbox" id="remember" className="mr-2"/>
-                <label htmlFor="remember" className="text-gray-700">Remember me</label>
-              </div>
-
               <button className="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition-colors mb-4">
-                Sign up
+                Send Message
               </button>
-
-              <hr className="my-4 border-gray-300" />
-              <small className="text-gray-500">
-                By clicking Sign up, you agree to the terms of use.
-              </small>
+              
             </form>
           </div>
         </div>
