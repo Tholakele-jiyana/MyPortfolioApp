5b5b3da4e931350e597b7039d5f5a6574744db55
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-10-29 21:27:19 +0200

    Updated the content of the page

--- Portfolio-App/src/Components/About.jsx
+++ Portfolio-App/src/Components/About.jsx
@@ -3,12 +3,20 @@ function About() {
     <section className="bg-gray-100 py-24">
       <div className="text-center px-6">
         <h1 className="text-5xl font-extrabold text-gray-900 mb-6">
-          Welcome to LT's Portfolio
+          About Me
         </h1>
 
         <div className="max-w-2xl mx-auto">
           <p className="text-lg text-gray-600 mb-10">
-            My name is Lindokuhle T Jiyana. I design and develop websites thats are visually appealling and user friendly. 
+            I am a Software Development student currently enrolled at Melsoft Academy. 
+            I am inquisitive about the evolving digital space; which is why I decided to pursue a career in tech. <br />
+            I enjoy learning about the new technologies and how they have become useful in solving
+            real world problems. <br />
+            The desire to shift from my previous career to tech is both exciting and challenging.
+            I am looking forward to gaining more knowledge, designing and developing as I progress and find my niche in the field. <br />
+
+            Outside of my day-to-day job and studies, I enjoy spending time with family and friends over a good meal.
+            Anything with to do with vacation, having time-out while indulging in sweet treats is always welcome in my world. 
           </p>
 
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
