be0d489792b6b478a71d629d7a289d0cca7c296a
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-10-29 20:46:35 +0200

    Updated the name of Portfolio

--- Portfolio-App/src/Components/Header.jsx
+++ Portfolio-App/src/Components/Header.jsx
@@ -1,15 +1,21 @@
 function Header() {
   return (
-    <div>
-        <h1>Header-NavBar Content</h1>
-        <p>This would ideally be the section where I put the navigation link</p>
-        <p>This will include all the components I have added:</p>
-        <h3>About</h3>
-        <h3>Hero</h3>
-        <h3>Projects</h3>
-        <h3>Contact</h3>
-        
-    </div>
+    <nav className="bg-gray-900 text-white px-6 py-4">
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
+        {/* Brand */}
+        <a href="#" className="text-xl font-bold">LT-Portfolio</a>
+
+        {/* Navigation Links */} 
+        <ul className="flex space-x-6">
+          <li><a href="#home" className="hover:text-indigo-400">Home</a></li>
+          <li><a href="#features" className="hover:text-indigo-400">About</a></li>
+          <li><a href="#pricing" className="hover:text-indigo-400">Projects</a></li>
+          <li><a href="#pricing" className="hover:text-indigo-400">Contact</a></li>
+
+          
+        </ul>
+      </div>
+    </nav>
   );
 }
 
