e64d5c5f18e6ff18628aff833e23e7a3c8c06723
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-10-29 20:40:54 +0200

    Chnaged the Hero section to Home

---
+++ Portfolio-App/src/Components/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+        <h1>Hero</h1>
+        <p>The hero section will provide introduction information about the webpage and perhaps a brief description.
+        </p>
+    </div>
+  );
+}
+
+export default Home;
