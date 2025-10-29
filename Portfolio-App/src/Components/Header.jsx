618bf817a9ae43f9b265658a635ebaad7b1e62af
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-10-30 01:47:31 +0200

    Href links updated from pre built text

--- Portfolio-App/src/Components/Header.jsx
+++ Portfolio-App/src/Components/Header.jsx
@@ -6,12 +6,12 @@ function Header() {
         {/* Brand */}
         <a href="#" className="text-xl font-bold">LT-Portfolio</a>
 
-        {/* Navigation Links text changed to black */} 
+        {/* Navigation Links text changed to black and href links updated to be same as link*/} 
         <ul className="text-black flex space-x-6">
-          <li><a href="#home" className="hover:text-indigo-400">Home</a></li>
-          <li><a href="#features" className="hover:text-indigo-400">About</a></li>
-          <li><a href="#pricing" className="hover:text-indigo-400">Projects</a></li>
-          <li><a href="#pricing" className="hover:text-indigo-400">Contact</a></li>
+          <li><a href="#Home" className="hover:text-indigo-400">Home</a></li>
+          <li><a href="#About" className="hover:text-indigo-400">About</a></li>
+          <li><a href="#Projects" className="hover:text-indigo-400">Projects</a></li>
+          <li><a href="#Contact" className="hover:text-indigo-400">Contact</a></li>
 
           
         </ul>
